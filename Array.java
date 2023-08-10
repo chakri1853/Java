3cb3eeb676aeaebf49ad2da4cd92e34162d41f95
chakri1853 <chakri.yamala@gmail.com>
2023-08-10 15:45:40 +0530

    Array

--- Array.java
+++ Array.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class Array {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size ");
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        int[] arr = new int[n];
         System.out.println("enter the numbers");
         for(int i=0;i<n;i++)
                 arr[i] = sc.nextInt();
